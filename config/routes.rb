239bd09733d1095babf3b0addfaafa4ec39b6286
brandon <callmebrandon@gmail.com>
2013-09-16 15:26:09 -0400

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Omrails::Application.routes.draw do
+  get 'about' => "pages#about"
+  
+root :to => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   resources :tests
 
   # You can have the root of your site routed with "root"
-  root 'tests#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
